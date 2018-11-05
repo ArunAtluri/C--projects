5af38c50b208b1176f168826f4e7b8d840c4b375
Arun Atluri <atluriarun@gmail.com>
2018-11-05 15:44:41 +1300

    Simple Calculator Project
    Using C# code.

---
+++ Calculator/Input.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Input
+    {
+        public double InputNumber()
+        {
+            bool isConverted = true;
+            double givenNumber;
+            while (true)
+            {
+                //Console.WriteLine("Enter the value");
+                String value = Console.ReadLine();
+                isConverted = double.TryParse(value, out givenNumber);
+
+                if (!isConverted)
+                {
+                    Console.WriteLine("The input must be numeric");
+                    continue;
+                }
+                break;
+            }
+            
+
+            return givenNumber;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            Input input = new Input();
+            //var firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st number");
+            double firstNumber = input.InputNumber();
+
+            Console.WriteLine("Enter the 2nd number");
+            double secondNumber = input.InputNumber();
+            
+            Console.WriteLine("Enter the operation");
+            var operation = Console.ReadLine().ToLower();
+
+            double result = 0;
+
+            if (operation.Equals("multiply") || operation.Equals("*"))
+            {
+                result = (firstNumber) * (secondNumber);
+                Console.WriteLine("The result is " + result);
+            }
+            else if(operation.Equals("addition") || operation.Equals("+"))
+            {
+                result = (firstNumber) + (secondNumber);
+                Console.WriteLine("The result is " + result);
+            }
+            else if (operation.Equals("subract") || operation.Equals("-"))
+            {
+                result = (firstNumber) - (secondNumber);
+                Console.WriteLine("The result is " + result);
+            }
+            else if (operation.Equals("division") || operation.Equals("/"))
+            {
+                result = (firstNumber) / (secondNumber);
+                Console.WriteLine("The result is " + result);
+            }
+            else if (operation.Equals("modulo") || operation.Equals("%"))
+            {
+                result = (firstNumber) % (secondNumber);
+                Console.WriteLine("The result is " + result);
+            }
+            else
+            {
+                Console.WriteLine("The result is unknown, invalid operation.");
+            }
+         
+        }
+    }
+}
